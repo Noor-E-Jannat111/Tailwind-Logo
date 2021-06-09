eb09dfacc39cae9899a29d969dadecd92503c757
noorejannat111 <noorejannat188@gmail.com>
2021-06-09 21:25:08 +0600

    done

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      color:{
+        'stra':#f14a60,
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
